1945516051ba6a49fd4b47dc3bb1eeadaa51425a
Bryanwebprogrammer2012 <119443381+Bryanwebprogrammer2012@users.noreply.github.com>
2022-12-06 15:00:54 +0300

    Add files via upload

---
+++ Examination project.py
@@ -0,0 +1,45 @@
+print("\nEXAMINATION RESULTS")
+
+marks = input("\nEnter pupil 1's grade: ")
+if marks>= "70":
+    print("\nYou Exceeded expectations.")
+elif marks >= "50" and marks < "70":
+    print("\nYou are Meeting expectations.")
+elif marks >= "20" and marks < "40":
+    print("\nYou are Average in expectations.")
+elif marks >= "0" and marks < "20":
+    print("\nYou are Below expectations.")
+
+
+marks = input("\nEnter pupil 2's grade: ")
+if marks>= "70":
+    print("\nYou Exceeded expectations.")
+elif marks >= "50" and marks < "70":
+    print("\nYou are Meeting expectations.")
+elif marks >= "20" and marks < "40":
+    print("\nYou are Average in expectations.")
+elif marks >= "0" and marks < "20":
+    print("\nYou are Below expectations.")
+ 
+
+marks = input("\nEnter pupil 3's grade: ")
+if marks>= "70":
+    print("\nYou Exceeded expectations.")
+elif marks >= "50" and marks < "70":
+    print("\nYou are Meeting expectations.")
+elif marks >= "20" and marks < "40":
+    print("\nYou are Average in expectations.")
+elif marks >= "0" and marks < "20":
+    print("\nYou are Below expectations.")
+ 
+
+marks = input("\nEnter pupil 4's grade: ")
+if marks>= "70":
+    print("\nYou Exceeded expectations.")
+elif marks >= "50" and marks < "70":
+    print("\nYou are Meeting expectations.")
+elif marks >= "20" and marks < "40":
+    print("\nYou are Average in expectations.")
+elif marks >= "0" and marks < "20":
+    print("\nYou are Below expectations.")
+ 
